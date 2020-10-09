aeabcfc48abf81c81e978a258674d3ebea313a41
selam lemma <oneiconforlife26@gmail.com>
2020-10-09 14:58:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,8 +2,7 @@ var chocolateBars = ["snickers" ,"hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element){
   ["array", ...element]
-  console.log(array);
-  return array;
+
 }
 function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element)
