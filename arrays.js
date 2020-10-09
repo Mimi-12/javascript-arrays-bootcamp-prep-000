5716e0c639ef5b469aea7ba2c1e6bf02a210cb14
selam lemma <oneiconforlife26@gmail.com>
2020-10-09 15:43:54 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -37,6 +37,6 @@ function  destructivelyRemoveElementFromEndOfArray(array){
   return array;
 }
 function removeElementFromEndOfArray(array){
-  ary = array.slice((1,array.length-1))
+  array.slice((1,array.length-1))
   return ary;
 }
