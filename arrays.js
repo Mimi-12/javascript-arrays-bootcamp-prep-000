0e68e7208696703741ce2f6b06ce21827b4b04d7
selam lemma <oneiconforlife26@gmail.com>
2020-10-09 15:14:49 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,7 +11,7 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 }
 function addElementToEndOfArray(array, element){
  arry = [element,...array]
-  return arry;
+ return arry;
 }
 function destructivelyAddElementToEndOfArray(array, element){
   array.push(element)
