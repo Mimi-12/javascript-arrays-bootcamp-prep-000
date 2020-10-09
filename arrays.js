3dcecdf15462c74cc43a327ef7f7025f40f85170
selam lemma <oneiconforlife26@gmail.com>
2020-10-09 14:58:19 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers" ,"hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element){
-  array.unshift()
+  ["array", ...element]
   console.log(array);
   return array;
 }
