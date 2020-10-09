a5c2fb8c6790c69e2276d6330d115470890a42df
selam lemma <oneiconforlife26@gmail.com>
2020-10-09 14:05:17 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,49 @@
+var chocolateBars = ["snickers" ,"hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  console.log(array);
+  return array;
+
+}
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  console.log(array);
+  return array;
+}
+function addElementToEndOfArray(array, element){
+ array.push(element)
+  console.log(array);
+  return array;
+}
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  console.log(array);
+  return array;
+}
+function  accessElementInArray(array, index){
+  array = [array.index]
+  console.log(array[0]);
+}
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  console.log(array);
+  return array;
+}
+function removeElementFromBeginningOfArray(array){
+  array.shift
+  console.log(array);
+  return array;
+}
+function  destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  console.log(array);
+  return array;
+
+}
+function removeElementFromEndOfArray(array){
+  array.pop
+  console.log(array);
+  return array;
+
+}
